28d5c4fcfd89500198d44513354007ada6ef47a4
Joyfull-Happiness <lajoie.bradley2@gmail.com>
2025-10-29 10:42:24 -0700

    updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 // ---------------------------------
 // Boilerplate Code to Set Up Server
 // ---------------------------------
+//importing all of our Node Modules
+import express from "express";
 
+//Creating an instance of express so we can use all of the methods, functions, properties of express
+const app = express();
+
+//defining our promptnumber
+const port = 3000;
+
+//
 // ---------------------------------
 // Helper Functions
 // ---------------------------------
@@ -11,7 +20,7 @@
 // ---------------------------------
 
 // --------------------------------
-// 🚀 LEVEL 1 CHALLENGES 
+// 🚀 LEVEL 1 CHALLENGES
 // --------------------------------
 
 // 1. 🏆 Add a /goodbye endpoint that responds with "Goodbye, see you later!"
@@ -29,16 +38,16 @@
 //      - If language = "Spanish", respond with "Hola, [name]!"
 //      - If language = "Vietnamese", respond with "Xin chao, [name]!"
 //      - If language = "Turkish", respond with "Merhaba, [name]!"
-//      - Add as many languages as you want! 
-//      - Otherwise, respond with "Language not supported."" 
+//      - Add as many languages as you want!
+//      - Otherwise, respond with "Language not supported.""
 
 // --------------------------------
 // 🚀 LEVEL 3 CHALLENGES — EVEN MORE DYNAMIC PARAMETERS
 // --------------------------------
 
-// 1. 🏆 Add a /calculate-dog-years/:dogName/:humanYears endpoint that calculates a dog's age in dog years. Refer to your dogAgeCalculator.js file. 
+// 1. 🏆 Add a /calculate-dog-years/:dogName/:humanYears endpoint that calculates a dog's age in dog years. Refer to your dogAgeCalculator.js file.
 
-// 2. 🏆 Add a /calculate-tip/:bill/:tipPercentage/:numGuests endpoint that calculates the amount each guests owes. Refer to your tipCalculator.js file. 
+// 2. 🏆 Add a /calculate-tip/:bill/:tipPercentage/:numGuests endpoint that calculates the amount each guests owes. Refer to your tipCalculator.js file.
 
 // --------------------------------
 // LEVEL 4 CHALLENGES — USING THE FILE SYSTEM MODULE
@@ -48,7 +57,7 @@
 
 // 2. 🏆 Add a /get-all-pizza-orders endpoint that responds with the array of pizza orders. Use the pizza-orders-data.json file in this folder.
 
-// 3. 🏆 Add a /get-one-pizza-order/:index endpoint that responds with the specified pizza order. 
+// 3. 🏆 Add a /get-one-pizza-order/:index endpoint that responds with the specified pizza order.
 
 // --------------------------------
 // 🚀 LEVEL 5 CHALLENGES — USING THIRD-PARTY MODULES
@@ -57,4 +66,3 @@
 // 1. 🏆 Add a /is-leap-year/:year endpoint that responds with whether the specified year is a leap year. Use the moment third-party node module and refer to your leap-year.js file.
 
 // 2. 🏆 Add a /get-signs/:month/:day/:year endpoint that responds with the user's astrological and zodiac signs based on their inputted birthday. Use the horoscope third-party node module and refer to your sign-finder.js file.
-
