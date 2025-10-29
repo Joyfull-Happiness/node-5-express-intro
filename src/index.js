23c1d7365c477d6de53b59c2326d88cd4eb9e220
Joyfull-Happiness <lajoie.bradley2@gmail.com>
2025-10-29 14:12:36 -0700

    udpated answer

--- src/index.js
+++ src/index.js
@@ -10,8 +10,15 @@ const app = express();
 //defining our promptnumber
 const port = 3000;
 
-//
-// ---------------------------------
+//Declaring that this server will be recieveing and responding to requests in JSON
+app.use(express.json());
+
+//Turn on our server so that it can listen for requests and respond to them at the port number
+app.listen(port, () => {
+  console.log(`The server is listening on port: ${port}`);
+});
+
+// ------------------------------
 // Helper Functions
 // ---------------------------------
 
@@ -19,19 +26,71 @@ const port = 3000;
 // Our very first API Endpoints
 // ---------------------------------
 
+//app.get() takes in 2 parameters:
+// 1. The endpoint
+//2.  Th callback funstion -what we do when we recieve a request at this endpoint, which takse in 2 parameters: the request (req) and reposnse (res) objects
+
+app.get("/", (req, res) => {
+  //the res.send() method sends back a string as a response
+
+  res.send("Hello World!");
+});
+
+app.get("/say-good-morning", (req, res) => {
+  //the res.send() method sends back a string as a response
+
+  res.send("Good Morning!");
+});
+
+app.get("/get-user", (req, res) => {
+  //the res.send() method sends back a string as a response
+  res.send("Hello Wednesday!");
+});
+
+//lets add a dynamic parameter
+// http://localhost:3000/get-user/Xavier -> Hello, Xavier!
+// http://localhost:3000/get-user/Nicole -> Hello, Nicole!
+
+app.get("/get-user/:username", (req, res) => {
+  //the res.send() method sends back a string as a response
+  res.send(`Hello, ${userName}!`);
+});
+
+app.get("/order-tacos/:protein/:numTacos", (req, res) => {
+  const protein = req.params.protein;
+  const numTacos = req.params.numTacos;
+
+  res.send(`Thanks for your order! You ordered ${numTacos} ${protein} tacos.`);
+});
 // --------------------------------
 // 🚀 LEVEL 1 CHALLENGES
 // --------------------------------
 
 // 1. 🏆 Add a /goodbye endpoint that responds with "Goodbye, see you later!"
 
+app.get("/goodbye", (req, res) => {
+  //the res.send() method sends back a string as a response
+  res.send("Goodbye, See You Later!");
+});
+
 // 2. 🏆 Add a /happy-birthday endpoint that responds with "Happy birthday!!!"
+
+app.get("/happy-birthday", (req, res) => {
+  //the res.send() method sends back a string as a response
+  res.send("Happy birthday!");
+});
 
 // --------------------------------
 // 🚀 LEVEL 2 CHALLENGES — ADDING DYNAMIC PARAMETERS
 // --------------------------------
 
 // 1. 🏆 Add a /happy-birthday/:name endpoint says "Happy birthday, [name]!!!"
+
+app.get("/happy-birthday/:name", (req, res) => {
+  const name = "jj";
+  //the res.send() method sends back a string as a response
+  res.send(`Happy birthday, ${name}!`);
+});
 
 // 2. 🏆 Add a /say-hello/:name/:language endpoint that says hello in multiple languages. Examples:
 //      - If language = "English", respond with "Hello, [name]!"
@@ -40,6 +99,25 @@ const port = 3000;
 //      - If language = "Turkish", respond with "Merhaba, [name]!"
 //      - Add as many languages as you want!
 //      - Otherwise, respond with "Language not supported.""
+
+app.get("/say-hello/:name/:language", (req, res) => {
+  const name = req.params.name;
+  const language = req.params.language;
+  //   const name = "CC";
+  //   const language = "Spanish";
+
+  if (language === "english") {
+    res.send(`Hello, ${name}!`);
+  } else if (language === "spanish") {
+    res.send(`Hola, ${name}`);
+  } else if (language === "vietnamese") {
+    res.send(`Xin chao, ${name}`);
+  } else if (language === "turkish") {
+    res.send(`Merhaba, ${name}`);
+  } else {
+    res.send("Language not supported.");
+  }
+});
 
 // --------------------------------
 // 🚀 LEVEL 3 CHALLENGES — EVEN MORE DYNAMIC PARAMETERS
